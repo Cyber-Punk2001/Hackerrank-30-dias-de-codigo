5eafca59251190603442161ac78f2331029d59dd
Cyber-Punk2001 <saudylebron54@gmail.com>
2026-07-18 11:01:06 +0000

    completando los retos del 0 al 3

--- hackerrank/day1/obj/Debug/net10.0/day1.AssemblyInfo.cs
+++ hackerrank/day1/obj/Debug/net10.0/day1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c402d7c81c5dbeb88faa9f1a86e1bc9643851b4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17cbea96b822b210ce64d45300b4a3b6675887dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
